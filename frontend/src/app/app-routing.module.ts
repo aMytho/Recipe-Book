f9cd85115abc945a425e5abf82732e4c8451481d
MythoAway <jshull04424@stu.usd313.org>
2022-02-09 18:45:15 +0000

    Start table view

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RecipeComponent } from './recipe/recipe.component';
+import { TableComponent } from './table/table.component';
 
 const routes: Routes = [
   {
     path: "recipe", component: RecipeComponent
+  },
+  {
+    path: "table", component: TableComponent
   }
 ];
 
