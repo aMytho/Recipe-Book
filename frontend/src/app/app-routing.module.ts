e7a45a2fab1a3e6eabd6ba49725a03d70f2643ef
MythoAway <jshull04424@stu.usd313.org>
2022-02-14 16:10:30 +0000

    Navabar and recipe list changes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
 import { RecipeComponent } from './recipe/recipe.component';
 import { TableComponent } from './table/table.component';
 
@@ -9,6 +10,9 @@ const routes: Routes = [
   },
   {
     path: "table", component: TableComponent
+  },
+  {
+    path: "list", component: RecipeListComponent
   }
 ];
 
