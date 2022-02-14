9ac66e8faf7f856a4d83763f42c2fbdae9b09900
MythoAway <jshull04424@stu.usd313.org>
2022-02-14 16:10:02 +0000

    Use recipe data to fill table

--- frontend/src/app/table/table.component.ts
+++ frontend/src/app/table/table.component.ts
@@ -7,65 +7,35 @@ import { RecipeService } from '../recipe/recipe.service';
   styleUrls: ['./table.component.scss']
 })
 export class TableComponent implements OnInit {
+  dataSource:any
   constructor(
     private recipeService: RecipeService
   ) { }
 
   ngOnInit(): void {
-    /*
-    this.recipeService.getAllRecipes().then(data => {
-      this.recipes = data.map((recipe: Recipe) => {
-        return {name: recipe.name, description: recipe.description}
+    this.recipeService.getAllRecipes().then((data:Recipe[]) => {
+      this.dataSource = data.map((element: Recipe) => {
+        return {name: element.name, totalCookTime: element.totalCookTime, description: element.description}
       });
-      alert(this.recipes);
-    }) */
+    })
   }
 
   columns = [
     {
-      columnDef: 'position',
-      header: 'No.',
-      cell: (element: PeriodicElement) => `${element.position}`,
-    },
-    {
       columnDef: 'name',
-      header: 'Name',
-      cell: (element: PeriodicElement) => `${element.name}`,
+      header: 'Name.',
+      cell: (recipe: Recipe) => `${recipe.name}`,
     },
     {
-      columnDef: 'weight',
-      header: 'Weight',
-      cell: (element: PeriodicElement) => `${element.weight}`,
+      columnDef: 'cook time',
+      header: 'Cook Time',
+      cell: (recipe: Recipe) => `${recipe.totalCookTime}`,
     },
     {
-      columnDef: 'symbol',
-      header: 'Symbol',
-      cell: (element: PeriodicElement) => `${element.symbol}`,
-    },
+      columnDef: 'description',
+      header: 'Decription',
+      cell: (recipe: Recipe) => `${recipe.description}`
+    }
   ];
-  dataSource = ELEMENT_DATA;
   displayedColumns = this.columns.map(c => c.columnDef);
-
-  
-
 }
-
-
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
