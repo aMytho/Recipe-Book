8c8ccb21269e278c8bb1f4c4abfacce62ef8f272
MythoAway <jshull04424@stu.usd313.org>
2022-03-23 18:05:02 +0000

    Add tailwind config

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    theme: {
+      // ...
+    },
+    content: [
+      "./src/**/*.{html,ts}",
+    ],
+    plugins: [
+      require('@tailwindcss/typography'),
+      // ...
+    ],
+  }
